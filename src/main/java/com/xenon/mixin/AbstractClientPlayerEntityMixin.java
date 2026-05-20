45a1277050a7d264cf9b5f0b4cfd40fb73966e16
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-05-20 15:54:09 +0300

    Update AbstractClientPlayerEntityMixin.java

--- src/main/java/com/xenon/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/com/xenon/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,17 +1,9 @@
 package com.xenon.mixin;
 
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.util.SkinTextures;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class AbstractClientPlayerEntityMixin {
-    
-    // Clasa rămâne goală momentan dacă nu mai ai alte mixin-uri în ea,
-    // prevenind crash-urile cauzate de SkinChanger-ul lipsă.
-
+    // Curat, fără importuri greșite și fără metode care dau erori
 }
