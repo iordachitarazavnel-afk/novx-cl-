ececc0a3cbf0de4454858378c094ea7db2c16511
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-05-20 15:55:56 +0300

    Update ChunkFinderV2.java

--- src/main/java/com/xenon/module/modules/donut/ChunkFinderV2.java
+++ src/main/java/com/xenon/module/modules/donut/ChunkFinderV2.java
@@ -440,7 +440,7 @@ public final class ChunkFinderV2 extends Module {
             int x = chunkPos.getStartX() + 8;
             int z = chunkPos.getStartZ() + 8;
 
-            mc.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F));
+            mc.getSoundManager().play(new PositionedSoundInstance(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP.value(), net.minecraft.sound.SoundCategory.MASTER, 1.0F, 1.0F, net.minecraft.util.math.random.Random.create(), mc.player.getX(), mc.player.getY(), mc.player.getZ()));
 
             if (mc.getToastManager() != null) {
                 mc.getToastManager().add(new ChunkFinderToast(
