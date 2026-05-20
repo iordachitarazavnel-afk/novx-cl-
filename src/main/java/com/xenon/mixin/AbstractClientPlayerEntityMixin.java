4221878674a52d1714574f644d26a3671571e950
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-05-20 15:51:58 +0300

    Update AbstractClientPlayerEntityMixin.java

--- src/main/java/com/xenon/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/com/xenon/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,21 +1,17 @@
 package com.xenon.mixin;
 
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.entity.player.SkinTextures;
+import net.minecraft.client.util.SkinTextures;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class AbstractClientPlayerEntityMixin {
+    
+    // Clasa rămâne goală momentan dacă nu mai ai alte mixin-uri în ea,
+    // prevenind crash-urile cauzate de SkinChanger-ul lipsă.
 
-    @Inject(method = "getSkin", at = @At("HEAD"), cancellable = true)
-    private void xenon$overrideOwnSkin(CallbackInfoReturnable<SkinTextures> cir) {
-         //AbstractClientPlayerEntity self = (AbstractClientPlayerEntity) (Object) this;
-        // SkinTextures override = SkinChanger.getOverrideSkin(self.getUuid());
-        // if (override != null) {
-            // cir.setReturnValue(override);
-        }
-    }
 }
